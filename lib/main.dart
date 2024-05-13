a9451ceb29491f8ca739c85ba2168cde68407657
Nabil Chebbah <chebbah.nabilkarim@gmail.com>
2024-05-13 14:57:05 +0100

    seven

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo 2',
+
+      title: 'Flutter Demo 7',
+
       theme: ThemeData(
         // This is the theme of your application.
         //
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,15 @@
 package org.example;public class Main {
+<<<<<<< HEAD
     public static void main(String[] args ) {
 
+=======
+<<<<<<< HEAD
+    public static void main(String[] args) {
+        System.out.println("Hello world!22");
+=======
+    public static void main(String[] args ) {
+
+>>>>>>> 77150b5 (two)
+>>>>>>> 5b0c4b9 (six)
     }
 }
