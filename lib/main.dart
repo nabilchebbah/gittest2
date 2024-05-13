067f7e9b402ddd0fe04e385d7da955f7ddcf8e5f
Nabil Chebbah <chebbah.nabilkarim@gmail.com>
2024-05-13 16:21:39 +0100

    ten

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
 
-      title: 'Flutter Demo 8',
+      title: 'Flutter Demo 10',
 
       theme: ThemeData(
         // This is the theme of your application.
